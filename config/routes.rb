da14ddcf943998b8dce505f2ac91658db388c3e2
SeaOfLee <l.richardson1@gmail.com>
2015-02-17 23:18:54 -0800

    Comment modal works from post#show

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   
-resources :posts
+resources :posts do
+  resources :comments, only: [:create, :destroy]
+end  
 
 resource :sessions, only: [:new, :create, :destroy]
 
